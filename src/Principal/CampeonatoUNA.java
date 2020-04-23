be8a7f17654c71fb0966dc9f000db0ba5066cf4a
Joshua99SG <64177422+Joshua99SG@users.noreply.github.com>
2020-04-22 19:45:13 -0600

    Update CampeonatoUNA.java
    Prueba 2

--- src/Principal/CampeonatoUNA.java
+++ src/Principal/CampeonatoUNA.java
@@ -17,6 +17,7 @@ public class CampeonatoUNA {
     public static void main(String[] args) {
         // TODO code application logic here
         //Hola muchachos
+        //Hola muchachos x2
     }
     
 }
